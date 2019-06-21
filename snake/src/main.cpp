7a3898d283c449b1315252f690f00ea5a22c97fc
andres linclau <andres.linclau@gmail.com>
2019-06-21 14:29:48 +0200

    cpp uploaden

---
+++ snake/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+bool gameOver;
+const int hooghte = 20;
+const int breedte = 20;
+int x, y, fruitX, fruitY, score;
+void setup()
+{
+	gameOver = false;
+	//fruitX = rand() % breedte;
+	//fruitY = rand() % hooghte;
+}
+
+void draw()
+{
+}
+
+void input()
+{
+}
+
+void loqic()
+{
+}
+using namespace std;
+int main()
+{
+	setup();
+	while (!gameOver)
+	{
+		cout << "test";
+	}
+}
