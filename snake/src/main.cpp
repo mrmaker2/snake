3997624f01d304c5944f978b92f265bfcd25b4f7
andres linclau <andres.linclau@gmail.com>
2019-06-21 16:47:05 +0200

    bezig met de muur

--- snake/src/main.cpp
+++ snake/src/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+using namespace std;
 bool gameOver;
 const int hooghte = 20;
 const int breedte = 20;
 int x, y, fruitX, fruitY, score;
+
 void setup()
 {
 	gameOver = false;
+	x = breedte / 2;
+	y = hooghte / 2;
 	//fruitX = rand() % breedte;
 	//fruitY = rand() % hooghte;
 }
 
 void draw()
 {
+	for (int i = 0; i < breedte; i++)
+
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < hooghte; i++)
+	{
+		for (int i = 0; i < hooghte; i++){
+			 
+		}
+
+	}
 }
 
 void input()
@@ -21,12 +37,14 @@ void input()
 void loqic()
 {
 }
-using namespace std;
+
 int main()
 {
 	setup();
 	while (!gameOver)
 	{
-		cout << "test";
+		draw();
+		input();
+		loqic();
 	}
 }
